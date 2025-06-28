9c5cb3857168b0c0bc863f7db7cafd2c1b58177d
Muhammed Mijwad <mijuzz007@gmail.com>
2025-06-28 15:37:39 +0530

    vercel set up

--- book_manager/settings.py
+++ book_manager/settings.py
@@ -48,6 +48,8 @@ INSTALLED_APPS = [
     'books',
 ]
 
+WSGI_APPLICATION = 'book_manager.wsgi.application'
+
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.SessionAuthentication',
@@ -151,6 +153,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
+STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
---
+++ build_files.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+D:/Work/Django/book_manager/venv/Scripts/python manage.py collectstatic --noinput
